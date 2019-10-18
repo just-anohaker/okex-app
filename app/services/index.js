09347264df60a7055e76468ea18dda7c6bf69c71
just-anohaker <just.anohaker@gmail.com>
2019-10-18 16:26:14 +0800

    commit

--- app/services/index.js
+++ app/services/index.js
@@ -54,7 +54,8 @@ function nativeUtils() {
                 title: "预警音乐文件选择",
                 properties: ["openFile"],
                 filters: [
-                    { name: "预警音乐", extensions: ["mp3", "wav", "ogg", "flac"] }
+                    { name: "Mp3音乐", extensions: ["mp3"] },
+                    { name: "Ogg音乐", extensions: ["ogg"] }
                 ]
             },
             (filePaths) => {
